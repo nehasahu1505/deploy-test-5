eed7633ea60151398b9f14652dedf91fe38fbbbb
Adrian Solis <aosolis@gmail.com>
2019-04-11 17:04:59 -0700

    Add application insights telemetry to the project (#5)
    - Add project to hold code that will be common to app templates, including telemetry
    - Add application insights settings to web.config
    - Configure Autofac to inject ILogProvider to MessagesController
    - Log basic information about incoming user activities

--- Microsoft.Teams.Celebration.App/App_Start/FilterConfig.cs
+++ Microsoft.Teams.Celebration.App/App_Start/FilterConfig.cs
@@ -1,10 +1,20 @@
-﻿using System.Web;
-using System.Web.Mvc;
+﻿// <copyright file="FilterConfig.cs" company="Microsoft">
+// Copyright (c) Microsoft. All rights reserved.
+// </copyright>
 
 namespace Microsoft.Teams.Celebration.App
 {
+    using System.Web.Mvc;
+
+    /// <summary>
+    /// Filter configurator
+    /// </summary>
     public class FilterConfig
     {
+        /// <summary>
+        /// Configures global filters
+        /// </summary>
+        /// <param name="filters">The filters to configure</param>
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             filters.Add(new HandleErrorAttribute());
--- Microsoft.Teams.Celebration.App/App_Start/WebApiConfig.cs
+++ Microsoft.Teams.Celebration.App/App_Start/WebApiConfig.cs
@@ -5,11 +5,13 @@
 namespace Microsoft.Teams.Celebration.App
 {
     using System.Web.Http;
+    using Autofac.Integration.WebApi;
+    using Microsoft.Bot.Builder.Dialogs;
     using Newtonsoft.Json;
     using Newtonsoft.Json.Serialization;
 
     /// <summary>
-    /// Represents Web Api configurations.
+    /// Web Api configurator
     /// </summary>
     public static class WebApiConfig
     {
@@ -29,6 +31,9 @@ namespace Microsoft.Teams.Celebration.App
                 Formatting = Formatting.Indented,
                 NullValueHandling = NullValueHandling.Include,
             };
+
+            // Web API configuration and services
+            config.DependencyResolver = new AutofacWebApiDependencyResolver(Conversation.Container);
 
             // Web API routes
             config.MapHttpAttributeRoutes();

6c27b92187b84856ef15b0b1adfbd0b5d4594b34
Neha Sahu <46955381+nehasahu1505@users.noreply.github.com>
2019-06-03 16:36:27 +0530

    Create required models to send the preview and event card. (#13)
    * Create required models to send the preview and event card.
    
    * Added JsonPropertyattribute on Model properties.
    
    * change id to read/write property
    
    * Fixed typo.
    
    * moved the information of last send attempt to an object
    
    * added missing file

---
+++ Microsoft.Teams.Celebration.App/Models/EventMessageActivity.cs
@@ -0,0 +1,50 @@
+﻿// <copyright file="EventMessageActivity.cs" company="Microsoft">
+// Copyright (c) Microsoft. All rights reserved.
+// </copyright>
+
+namespace Microsoft.Teams.Celebration.App.Models
+{
+    using System;
+
+    /// <summary>
+    /// Represents an activity to construct the celebration card.
+    /// </summary>
+    [Serializable]
+    public class EventMessageActivity
+    {
+        /// <summary>
+        /// Gets or sets id, which is Id(Guid) in events collection
+        /// </summary>
+        public string Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets event's owner name
+        /// </summary>
+        public string OwnerName { get; set; }
+
+        /// <summary>
+        /// Gets or sets event's title.
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Gets or sets event's Header.
+        /// </summary>
+        public string Header { get; set; }
+
+        /// <summary>
+        /// Gets or sets event's Message
+        /// </summary>
+        public string Message { get; set; }
+
+        /// <summary>
+        /// Gets or sets event's Image Url
+        /// </summary>
+        public string ImageUrl { get; set; }
+
+        /// <summary>
+        /// Gets or sets conversation Id that is required to intiate the conversation between user/team and bot.
+        /// </summary>
+        public string ConversationId { get; set; }
+    }
+}
